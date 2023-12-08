fcfd338da96026c4c8c663fc75080a9bf4e299d6
deveb1479@gmail.com <deveb1479@gmail.com>
2023-12-08 20:22:45 +0900

    ..

--- API01-OpenSSL/Program.cs
+++ API01-OpenSSL/Program.cs
@@ -1,25 +1,5 @@
-﻿using System.Security.Cryptography;
-using System.Text;
-
-public class Program
+﻿public partial class Program
 {
-    public enum Algorithm
-    {
-        DES,
-        AES,
-    }
-    public enum Rotate
-    {
-        Encrypt,
-        Decrypt,
-    }
-    public enum Mode
-    {
-        ECB,
-        CBC,
-        CFB,
-    }
-
     public static void Main(string[] args)
     {
         while (true)
@@ -35,147 +15,46 @@ public class Program
             FileStream stream;
             string password;
 
-            while (true)
+            try
             {
-                try
+                switch (cmds[0])
                 {
-                    switch (cmds[0])
-                    {
-                        case "des": algorithm = Algorithm.DES; break;
-                        case "aes": algorithm = Algorithm.AES; break;
+                    case "des": algorithm = Algorithm.DES; break;
+                    case "aes": algorithm = Algorithm.AES; break;
 
-                        default: throw new ArgumentException();
-                    }
-
-                    switch (cmds[1])
-                    {
-                        case "enc": rotate = Rotate.Encrypt; break;
-                        case "dec": rotate = Rotate.Decrypt; break;
-
-                        default: throw new ArgumentException();
-                    }
-
-                    switch (cmds[2])
-                    {
-                        case "ecb": mode = Mode.ECB; break;
-                        case "cbc": mode = Mode.CBC; break;
-                        case "cfb": mode = Mode.CFB; break;
-
-                        default: throw new ArgumentException();
-                    }
-
-                    stream = File.OpenRead(cmds[3]);
-
-                    password = cmds[4];
-
-                    break;
+                    default: throw new ArgumentException();
                 }
-                catch (Exception ex)
+
+                switch (cmds[1])
                 {
-                    Console.WriteLine(ex.Message);
+                    case "enc": rotate = Rotate.Encrypt; break;
+                    case "dec": rotate = Rotate.Decrypt; break;
 
-                    continue;
+                    default: throw new ArgumentException();
                 }
+
+                switch (cmds[2])
+                {
+                    case "ecb": mode = Mode.ECB; break;
+                    case "cbc": mode = Mode.CBC; break;
+                    case "cfb": mode = Mode.CFB; break;
+
+                    default: throw new ArgumentException();
+                }
+
+                stream = File.OpenRead(cmds[3]);
+
+                password = cmds[4];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                continue;
             }
 
             SecureService service = new SecureService(algorithm, rotate, mode, stream, password);
-        }
-    }
-
-    public class SecureService
-    {
-        public SecureService(Algorithm algorithm, Rotate rotate, Mode mode, FileStream stream, string password)
-        {
-            byte[] hashedPass = SHA512.HashData(Encoding.UTF8.GetBytes(password));
-            using BinaryReader reader = new BinaryReader(stream);
-            byte[] plain = reader.ReadBytes((int)stream.Length);
-            byte[] result;
-
-            switch (algorithm)
-            {
-                case Algorithm.DES:
-                    {
-                        TripleDES des = TripleDES.Create();
-                        des.Key = hashedPass[0..24];
-
-                        if (rotate == Rotate.Encrypt && mode == Mode.ECB)
-                        {
-                            result = des.EncryptEcb(plain, PaddingMode.PKCS7);
-                        }
-                        else if (rotate == Rotate.Decrypt && mode == Mode.ECB)
-                        {
-                            result = des.DecryptEcb(plain, PaddingMode.PKCS7);
-                        }
-                        else if (rotate == Rotate.Encrypt && mode == Mode.CBC)
-                        {
-                            result = des.EncryptCbc(plain, hashedPass[(64 - 8)..64]);
-                        }
-                        else if (rotate == Rotate.Decrypt && mode == Mode.CBC)
-                        {
-                            result = des.DecryptCbc(plain, hashedPass[(64 - 8)..64]);
-                        }
-                        else if (rotate == Rotate.Encrypt && mode == Mode.CFB)
-                        {
-                            result = des.EncryptCfb(plain, hashedPass[(64 - 8)..64]);
-                        }
-                        else if (rotate == Rotate.Decrypt && mode == Mode.CFB)
-                        {
-                            result = des.DecryptCfb(plain, hashedPass[(64 - 8)..64]);
-                        }
-                        else
-                        {
-                            return;
-                        }
-
-                        string name = stream.Name.Split('.').Last() == "enc" ?
-                            stream.Name[0..(stream.Name.Length - 4)] : stream.Name + ".enc";
-
-                        using BinaryWriter writer = new BinaryWriter(File.Create(name));
-                        writer.Write(result);
-                    }
-                    break;
-                case Algorithm.AES:
-                    {
-                        Aes aes = Aes.Create();
-                        aes.Key = hashedPass[0..32];
-
-                        if (rotate == Rotate.Encrypt && mode == Mode.ECB)
-                        {
-                            result = aes.EncryptEcb(plain, PaddingMode.PKCS7);
-                        }
-                        else if (rotate == Rotate.Decrypt && mode == Mode.ECB)
-                        {
-                            result = aes.DecryptEcb(plain, PaddingMode.PKCS7);
-                        }
-                        else if (rotate == Rotate.Encrypt && mode == Mode.CBC)
-                        {
-                            result = aes.EncryptCbc(plain, hashedPass[(64 - 16)..64]);
-                        }
-                        else if (rotate == Rotate.Decrypt && mode == Mode.CBC)
-                        {
-                            result = aes.DecryptCbc(plain, hashedPass[(64 - 16)..64]);
-                        }
-                        else if (rotate == Rotate.Encrypt && mode == Mode.CFB)
-                        {
-                            result = aes.EncryptCfb(plain, hashedPass[(64 - 16)..64]);
-                        }
-                        else if (rotate == Rotate.Decrypt && mode == Mode.CFB)
-                        {
-                            result = aes.DecryptCfb(plain, hashedPass[(64 - 16)..64]);
-                        }
-                        else
-                        {
-                            return;
-                        }
-
-                        string name = stream.Name.Split('.').Last() == "enc" ?
-                            stream.Name[0..(stream.Name.Length - 4)] : stream.Name + ".enc";
-
-                        using BinaryWriter writer = new BinaryWriter(File.Create(name));
-                        writer.Write(result);
-                    }
-                    break;
-            }
+            service.Run();
         }
     }
 }
---
+++ API01-OpenSSL/SecureService.cs
@@ -0,0 +1,146 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using static Program;
+
+public partial class Program
+{
+    public enum Algorithm
+    {
+        DES,
+        AES,
+    }
+    public enum Rotate
+    {
+        Encrypt,
+        Decrypt,
+    }
+    public enum Mode
+    {
+        ECB,
+        CBC,
+        CFB,
+    }
+    public enum Result
+    {
+        OK,
+        Error,
+    }
+
+    public class SecureService
+    {
+        Algorithm Algorithm;
+        Rotate Rotate;
+        Mode Mode;
+        byte[] HashedPass;
+        byte[] PlainText;
+        FileStream Stream;
+
+        public SecureService(Algorithm algorithm, Rotate rotate, Mode mode, FileStream stream, string password)
+        {
+            using BinaryReader reader = new BinaryReader(stream);
+            
+            Algorithm = algorithm;
+            Rotate = rotate;
+            Mode = mode;
+            Stream = stream;
+            HashedPass = SHA512.HashData(Encoding.UTF8.GetBytes(password));
+            PlainText = reader.ReadBytes((int)stream.Length);
+        }
+
+        public Result Run()
+        {
+            byte[] result;
+
+            switch (Algorithm)
+            {
+                case Algorithm.DES:
+                    {
+                        TripleDES des = TripleDES.Create();
+                        des.Key = HashedPass[0..24];
+
+                        if (Rotate == Rotate.Encrypt && Mode == Mode.ECB)
+                        {
+                            result = des.EncryptEcb(PlainText, PaddingMode.PKCS7);
+                        }
+                        else if (Rotate == Rotate.Decrypt && Mode == Mode.ECB)
+                        {
+                            result = des.DecryptEcb(PlainText, PaddingMode.PKCS7);
+                        }
+                        else if (Rotate == Rotate.Encrypt && Mode == Mode.CBC)
+                        {
+                            result = des.EncryptCbc(PlainText, HashedPass[(64 - 8)..64]);
+                        }
+                        else if (Rotate == Rotate.Decrypt && Mode == Mode.CBC)
+                        {
+                            result = des.DecryptCbc(PlainText, HashedPass[(64 - 8)..64]);
+                        }
+                        else if (Rotate == Rotate.Encrypt && Mode == Mode.CFB)
+                        {
+                            result = des.EncryptCfb(PlainText, HashedPass[(64 - 8)..64]);
+                        }
+                        else if (Rotate == Rotate.Decrypt && Mode == Mode.CFB)
+                        {
+                            result = des.DecryptCfb(PlainText, HashedPass[(64 - 8)..64]);
+                        }
+                        else
+                        {
+                            return Result.Error;
+                        }
+
+                        string name = Stream.Name.Split('.').Last() == "enc" ?
+                        Stream.Name[0..(Stream.Name.Length - 4)] : Stream.Name + ".enc";
+
+                        using BinaryWriter writer = new BinaryWriter(File.Create(name));
+                        writer.Write(result);
+
+                        return Result.OK;
+                    }
+                case Algorithm.AES:
+                    {
+                        Aes aes = Aes.Create();
+                        aes.Key = HashedPass[0..32];
+
+                        if (Rotate == Rotate.Encrypt && Mode == Mode.ECB)
+                        {
+                            result = aes.EncryptEcb(PlainText, PaddingMode.PKCS7);
+                        }
+                        else if (Rotate == Rotate.Decrypt && Mode == Mode.ECB)
+                        {
+                            result = aes.DecryptEcb(PlainText, PaddingMode.PKCS7);
+                        }
+                        else if (Rotate == Rotate.Encrypt && Mode == Mode.CBC)
+                        {
+                            result = aes.EncryptCbc(PlainText, HashedPass[(64 - 16)..64]);
+                        }
+                        else if (Rotate == Rotate.Decrypt && Mode == Mode.CBC)
+                        {
+                            result = aes.DecryptCbc(PlainText, HashedPass[(64 - 16)..64]);
+                        }
+                        else if (Rotate == Rotate.Encrypt && Mode == Mode.CFB)
+                        {
+                            result = aes.EncryptCfb(PlainText, HashedPass[(64 - 16)..64]);
+                        }
+                        else if (Rotate == Rotate.Decrypt && Mode == Mode.CFB)
+                        {
+                            result = aes.DecryptCfb(PlainText, HashedPass[(64 - 16)..64]);
+                        }
+                        else
+                        {
+                            return Result.Error;
+                        }
+
+                        string name = Stream.Name.Split('.').Last() == "enc" ?
+                        Stream.Name[0..(Stream.Name.Length - 4)] : Stream.Name + ".enc";
+
+                        using BinaryWriter writer = new BinaryWriter(File.Create(name));
+                        writer.Write(result);
+
+                        return Result.OK;
+                    }
+            }
+
+            return Result.Error;
+        }
+    }
+}
